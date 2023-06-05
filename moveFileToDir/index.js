f43d67df8902ac47d568553bac224f96691bea64
chenkongming <kongming.ckm@nonolive.com>
2023-06-05 14:53:12 +0800

    feat: 移动文件工具使用文件前面的时间作为匹配字段

--- moveFileToDir/index.js
+++ moveFileToDir/index.js
@@ -5,13 +5,15 @@ const fs = require("fs");
 const path = require('path');
 const fileURL = require("../conf");
 // 需要移动的文件名列表
-const list = ["它怕是听懂不能找男朋友了",]
+const list = [
+]
+
 // 移动到目录名
-const moveToDir = 'uped'
+const moveToDir = '版权主张'
 const readDir = fs.readdirSync(fileURL);
 for (let i = 0; i < readDir.length; i++) {
     for (let j = 0; j < list.length; j++) {
-        if (readDir[i].includes(list[j].slice(0,4))) {
+        if (readDir[i].slice(0,10)===list[j].slice(0,10).replace(/ /g,"-")) {
             list.splice(j, 1)
             let sourceFile = path.join(fileURL, readDir[i]);
             let destPath = path.join(fileURL, moveToDir, readDir[i]);
@@ -20,10 +22,10 @@ for (let i = 0; i < readDir.length; i++) {
                     if (err) throw err;
                     fs.stat(destPath,
                         function(err, stats) {
-                            if (err) throw err;
-                            console.log('moved ' + list[j]);
+                            if (err) console.error(err);
                         });
                 });
         }
     }
 }
+
